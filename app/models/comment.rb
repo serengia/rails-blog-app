531488cea8d45f730bb7b95eac0fc3961543cf62
James Serengia <jamesserengia@gmail.com>
2023-07-13 10:55:05 +0300

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,15 @@
 class Comment < ApplicationRecord
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+
+  after_save :update_post_comment_counter
+  after_destroy :decrement_post_comment_counter
+
+  def update_post_comment_counter
+    post.increment!(:comments_counter)
+  end
+
+  def decrement_post_comment_counter
+    post.decrement!(:comments_counter)
+  end
 end
