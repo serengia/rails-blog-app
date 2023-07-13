0ecc87798af47c4efc1f3078bfae1f2cad5e0ebe
James Serengia <jamesserengia@gmail.com>
2023-07-13 12:07:42 +0300

    Protected private methods in Like and Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ApplicationRecord
   after_save :update_post_comment_counter
   after_destroy :decrement_post_comment_counter
 
+  private
+  
   def update_post_comment_counter
     post.increment!(:comments_counter)
   end
