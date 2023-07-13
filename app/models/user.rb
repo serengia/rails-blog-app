229fe8f3bd9546a65e60097742c8cc4f9349ae7d
James Serengia <jamesserengia@gmail.com>
2023-07-13 13:11:55 +0300

    Add validation tom User, Post, and Comment models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   # Validations
+  validates :name, presence: true, length: { minimum: 3, maximum: 50,
+    too_long: '%<count>s characters is the maximum allowed' }
+  validates :posts_counter, numericality: { only_integer: true },
+      comparison: { greater_than_or_equal_to: 0 }
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
