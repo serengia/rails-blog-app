5c41a418c5b2d2c317e5daadad606971506e0680
James Serengia <jamesserengia@gmail.com>
2023-07-20 06:27:06 +0300

    Fixed bug on controller files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def login
+    # Logic here
+  end
+
+  def about
+    # userArr = User.all
+    @message = ['Number 1', 'number 2', 'number 3'] # testing
+  end
+end
