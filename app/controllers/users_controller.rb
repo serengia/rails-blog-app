36ff1b5e5b50cc19c14cd39e6317b0d35c4ce167
James Serengia <jamesserengia@gmail.com>
2023-07-21 17:21:15 +0300

    Updated the user and post controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.all
   end
 
-  def show
+  def show;
+    @posts = @user.three_most_recent_posts
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
-    @posts = Post.all
   end
 end
