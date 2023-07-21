76694a0eaf6e21a78bdf969947e6774080749c1f
James Serengia <jamesserengia@gmail.com>
2023-07-21 13:52:22 +0300

    Create a shared views folder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    # @user = User.find(params[:user_id])
-    # @posts = @user.posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-    # Logic here
+    @post = Post.find(params[:post_id])
   end
 end
