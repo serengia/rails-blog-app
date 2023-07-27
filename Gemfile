158d7e6e337f15eb0de4424106cfe3468aada592
James Serengia <jamesserengia@gmail.com>
2023-07-27 20:30:18 +0300

    Splited the integration tests files

--- Gemfile
+++ Gemfile
@@ -77,8 +77,8 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
   gem 'selenium-webdriver'
-  gem 'webdrivers'
+  # gem 'webdrivers'
 
   # Use database cleaner to reset the database between tests
-  gem 'database_cleaner'
+  # gem 'database_cleaner'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
-# rails_helper.rb
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -61,23 +60,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, type: :system) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
